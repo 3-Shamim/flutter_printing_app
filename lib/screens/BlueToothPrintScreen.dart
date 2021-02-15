5b5e6466fc4fc199a78f8a71eb313914136cffa6
Shamim <mdshamim723@gmail.com>
2021-02-15 12:05:36 +0600

    master branch

--- lib/screens/BlueToothPrintScreen.dart
+++ lib/screens/BlueToothPrintScreen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart' hide Image;
-import 'package:flutter_bluetooth_basic/flutter_bluetooth_basic.dart';
 
 class BlueToothPrintScreen extends StatefulWidget {
   static const routeName = "/bluetooth-print";
@@ -9,57 +8,6 @@ class BlueToothPrintScreen extends StatefulWidget {
 }
 
 class _BlueToothPrintScreenState extends State<BlueToothPrintScreen> {
-  String _devicesMsg;
-  BluetoothManager bluetoothManager = BluetoothManager.instance;
-  List<Map<String, dynamic>> printAbleData = [];
-  bool _connected = false;
-  BluetoothDevice _device;
-  String tips = 'no device connect';
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      Map<String, dynamic> arguments =
-          ModalRoute.of(context).settings.arguments;
-      printAbleData = arguments["data"];
-      initBluetooth();
-    });
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initBluetooth() async {
-    bluetoothManager.startScan(timeout: Duration(seconds: 4));
-
-    bool isConnected = await bluetoothManager.isConnected;
-
-    bluetoothManager.state.listen((state) {
-      switch (state) {
-        case 12:
-          setState(() {
-            _connected = true;
-            tips = 'connect success';
-          });
-          break;
-        case 10:
-          setState(() {
-            _connected = false;
-            tips = 'disconnect success';
-          });
-          break;
-        default:
-          break;
-      }
-    });
-
-    if (!mounted) return;
-
-    if (isConnected) {
-      setState(() {
-        _connected = true;
-      });
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -67,6 +15,6 @@ class _BlueToothPrintScreenState extends State<BlueToothPrintScreen> {
         appBar: AppBar(
           title: Text('Bluetooth print'),
         ),
-        body: Center(child: Text(tips)));
+        body: Center(child: Text("Bluetooth print")));
   }
 }
