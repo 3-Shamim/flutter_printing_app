2b584b7631cdd2348a575a7102b1fb8a5cc92b89
Shamim <mdshamim723@gmail.com>
2021-02-15 12:29:05 +0600

    bluetooth basic scanner not working

--- lib/route.dart
+++ lib/route.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/cupertino.dart';
-import 'package:printing_app/screens/BlueToothPrintScreen.dart';
 import 'package:printing_app/screens/BluetoothScannerScreen.dart';
 import 'package:printing_app/screens/HomeScreen.dart';
-import 'package:printing_app/screens/WifiPrintScreen.dart';
 
 Map<String, Widget Function(BuildContext)> routes = {
   HomeScreen.routeName: (_) => HomeScreen(),
-  BlueToothPrintScreen.routeName: (_) => BlueToothPrintScreen(),
-  WifiPrintScreen.routeName: (_) => WifiPrintScreen(),
   BluetoothScannerScreen.routeName: (_) => BluetoothScannerScreen(),
 };
